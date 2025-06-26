69ef380b659b80fe75e7a0a1501a997b2c53a30a
อาเจ <82790183+sakol289@users.noreply.github.com>
2025-06-26 23:06:28 +0700

    (Version 1.58)

--- core/module/client/payment.php
+++ core/module/client/payment.php
@@ -2193,6 +2193,30 @@ elseif ($method_name == "paymentv2"):
     var_dump($extras);
 
 
+        $curl = curl_init();
+
+        // Encode clientId:clientSecret เป็น Base64 สำหรับ Authorization header
+        $auth = base64_encode("{$extras['ClientID']}:{$extras['ClientSecret']}");
+    
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://suba.rdcw.co.th/v1/inquiry',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30, // ป้องกันการรอนานเกินไป
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => json_encode(['payload' => $_POST['idkey']]), // ใช้ json_encode เพื่อความปลอดภัย
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: Basic $auth",
+                'Content-Type: application/json'
+            ),
+        ));
+
+    $response = curl_exec($curl);
+    $data = json_decode($response);
+
 
 
 // if (empty($code) && $status2 = true && $status == true && $secret == $apiSecret):
