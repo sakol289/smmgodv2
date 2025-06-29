b09ada1c4a1e7fc5a1b81ca29521bffdb7006f34
อาเจ <82790183+sakol289@users.noreply.github.com>
2025-06-29 23:47:43 +0700

    (Version 1.96)

--- core/module/client/payment.php
+++ core/module/client/payment.php
@@ -2299,7 +2299,6 @@ elseif ($method_name == "paymentv2"):
                         $amount = ($payment["payment_amount"] + ($payment["payment_amount"] * $payment_bonus["bonus_amount"] / 100));
                     } else {
                         echo "[DEBUG] No bonus.<br>";
-                        echo "ttttt2 : " . $amount;
                         $amount = $payment["payment_amount"];
                         echo "ttttt3 : " . $amount;
                     }
