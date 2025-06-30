f82c13fe1d56ebb73e56e6c9a0dbf9c67ed061b6
อาเจ <82790183+sakol289@users.noreply.github.com>
2025-06-30 20:23:44 +0700

    (Version 2.62)

--- core/temp/admin/dashboard.php
+++ core/temp/admin/dashboard.php
@@ -1,5 +1,5 @@
 <?php include 'header.php'; ?>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> -->
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <style>
