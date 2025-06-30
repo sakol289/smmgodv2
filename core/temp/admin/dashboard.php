e01e0a4e25bb6e83f44f0ce965d653ea6ecdeeb6
อาเจ <82790183+sakol289@users.noreply.github.com>
2025-06-30 20:23:18 +0700

    (Version 2.61)

--- core/temp/admin/dashboard.php
+++ core/temp/admin/dashboard.php
@@ -420,7 +420,7 @@
       </div>
     </main>
   </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> -->
 
   <script>
     // Initialize charts when the page loads
