a07587083342c73722845e14910c8187dabaae64
อาเจ <82790183+sakol289@users.noreply.github.com>
2025-07-05 18:32:29 +0700

    (Version 3.34)

--- core/module/admin/appearance.php
+++ core/module/admin/appearance.php
@@ -12,6 +12,7 @@ if ($_SESSION["client"]["data"]):
   unset($_SESSION["client"]);
 endif;
 
+$menuList = ["Pages" => "pages", "News" => "news", "Blog" => "blog", "Menu" => "menu", "Themes" => "themes", "Language" => "language", "Files" => "files", "New Year" => "new_year"];
 
 if (!array_search(route(2), $menuList)):
   header("Location:" . site_url("admin/appearance"));
@@ -898,7 +899,7 @@ elseif (route(2) == "files"):
 
 
   if (route(2) == "new_year"):
-    $access = $admin["access"]["pages"];
+    $access = $user["access"]["themes"];
 
     if ($access):
       if ($_POST):
