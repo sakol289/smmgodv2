bfa061ecefe16209f2e280e29639269e343abc8f
อาเจ <82790183+sakol289@users.noreply.github.com>
2025-07-06 12:24:50 +0700

    (Version 3.47)

--- core/module/admin/ajax_data.php
+++ core/module/admin/ajax_data.php
@@ -9,11 +9,7 @@ $languages = $conn->prepare("SELECT * FROM languages WHERE language_type=:type")
 $languages->execute(["type" => 2]);
 $languages = $languages->fetchAll(PDO::FETCH_ASSOC);
 // Check if KEY constant is defined and valid
-if (!defined('KEY')) {
-    $return = "<div class=\"modal-body\"><center><h1>VOID PHP VERSION<h1></center></div>";
-    echo json_encode(["content" => $return, "title" => "Error"]);
-    exit;
-}
+
 if ($action == "providers_list") {
     $smmapi = new SMMApi();
     $provider = $_POST["provider"];
