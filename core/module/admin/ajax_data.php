4e85d22acc862f31e2a9fea8d73fef528342684f
อาเจ <82790183+sakol289@users.noreply.github.com>
2025-07-06 13:30:03 +0700

    (Version 3.64)

--- core/module/admin/ajax_data.php
+++ core/module/admin/ajax_data.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $action = $_POST["action"];
 $languages = $conn->prepare("SELECT * FROM languages WHERE language_type=:type");
 $languages->execute(["type" => 2]);
