c12e947f8cc3bf956a7e4345892754f5efcd28d7
อาเจ <82790183+sakol289@users.noreply.github.com>
2025-07-13 11:48:01 +0700

    (Version 5.06)

--- core/module/client/crons.php
+++ core/module/client/crons.php
@@ -1,6 +1,10 @@
 <?php
-echo "start";
-foreach ( glob(__DIR__.'/cron/*.php') as $cron ) {
-  require $cron;
+echo "start\n";
+
+foreach (glob(__DIR__.'/cron/*.php') as $cron) {
+    echo "Running: $cron\n";
+    require $cron;
+    echo "Done: $cron\n";
 }
-echo "end";
+
+echo "end\n";
